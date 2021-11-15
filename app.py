50fc004ba11bdc981d4ec0312ee9e84e2055c641
will118118 <william.irving@sky.uk>
2021-11-15 22:36:37 +0000

    added all the team members to one page and added an error page

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, redirect, abort
 
 app = Flask(__name__)
+
+members = 'Emeka', 'Kevin', 'Callum', 'Will'
 
 @app.route('/')
 def main_page ():
     return render_template('main.html')
 
-@app.route('/<name>')
+@app.route('/<name>/')
 def member (name):
-    return render_template("name.html", name=name)
+    if name in members:
+        return render_template("name.html", name=name)
+    else:
+        abort(404)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', errors=[error]), 404
 
     
---
+++ tests/testing.py
@@ -0,0 +1,6 @@
+import unittest
+import sys
+
+class Testnames (unittest.TestCase):
+    def Testspelling (self):
+        pass
