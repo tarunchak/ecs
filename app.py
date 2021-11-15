cafdb7722e58ddc877b7de9a207a59c129477d0f
will118118 <william.irving@sky.uk>
2021-11-15 21:24:57 +0000

    added a name template for each team member

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -6,14 +6,8 @@ app = Flask(__name__)
 def main_page ():
     return render_template('main.html')
 
-# @app.route('name/<name>')
-# def member_page(name):
-#     if name == "emeka":
-#         return render_template('name/emeka.html', name=name)
-#     if name == "kevin":
-#         return render_template('name/kevin', name=name)
-#     if name == "callum":
-#         render_template('name/callum', name=name)
-#     if name == "will":
-#         return render_template('name/will', name=name)
+@app.route('/<name>')
+def member (name):
+    return render_template("name.html", name=name)
+
     
